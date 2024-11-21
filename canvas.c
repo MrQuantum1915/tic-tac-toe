90e357c688a52bc5cafd2be86c767a57d97c9a8e
MrQuantum1915 <darshanpatelgdh@gmail.com>
2024-11-21 13:23:42 +0530

    added canvas

---
+++ canvas.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main () {
+
+    printf("\t   |   |   \n\t___|___|___\n\t   |   |   \n\t___|___|___\n\t   |   |   \n\t___|___|___\n\t   |   |   \n\t   |   |   ");
+    return 0;
+}
